6cb87746ae82a9e6418681e0225adc259e487836
Aruzhan6 <Arukamkat.99@mail.ru>
2020-12-17 15:20:52 +0600

    General information

---
+++ app/src/main/java/com/example/general_information/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.general_information;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
